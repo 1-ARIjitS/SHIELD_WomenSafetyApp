fd41fd0d6d0ae746906e5a69cf2309337d71cfe0
1-ARIjitS <arijits19@iiserb.ac.in>
2021-03-14 12:55:40 +0530

    about us activity fully completed backend and frontend all assets added to drawable calling intent and permissions added to about us activity and manifest respectively only transition between nav_bar buttons left to assign

---
+++ app/src/main/java/com/example/womensafety/Activities/AboutUs.java
@@ -0,0 +1,184 @@
+package com.example.womensafety.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.womensafety.Detail_Forms;
+import com.example.womensafety.Models.suspect_registered;
+import com.example.womensafety.R;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
+
+public class AboutUs extends AppCompatActivity {
+
+    ImageView linked_in;
+    ImageView instagram;
+    ImageView facebook;
+
+    TextView mobile_num;
+    TextView email_1;
+    TextView email_2;
+    TextView website;
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suspect_registration);
+
+        linked_in=findViewById(R.id.linkedin);
+        instagram=findViewById(R.id.instagram);
+        facebook=findViewById(R.id.facebook);
+
+        website=findViewById(R.id.website);
+
+        setUpToolbar();
+        navigationView = findViewById(R.id.navigationMenu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.nav_home:
+                        startActivity(new Intent(AboutUs.this, AdminActivity.class));
+                        break;
+
+                    case R.id.travellingALone:
+                        startActivity(new Intent(AboutUs.this, Detail_Forms.class));
+                        break;
+
+                    case R.id.nav_suspectRegistration:
+                        startActivity(new Intent(AboutUs.this, SuspectRegistrationActivity.class));
+                        break;
+                    case R.id.nav_nextToKin:
+                        startActivity(new Intent(AboutUs.this, NextToKinActivity.class));
+                        break;
+                    case R.id.nav_aboutUs:
+                        break;
+                }
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+
+
+        linked_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Url="https://www.linkedin.com/company/cybertronsoftwares/";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(Url));
+                startActivity(intent);
+            }
+        });
+        instagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Url="https://www.instagram.com/cybertronsoftwares/";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(Url));
+                startActivity(intent);
+            }
+        });
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Url="https://m.facebook.com/cybertronsoftware/?ti=as";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(Url));
+                startActivity(intent);
+            }
+        });
+
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Url="https://www.cybertronsoftwares.com/";
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(Url));
+                startActivity(intent);
+            }
+        });
+
+        mobile_num.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:7774050060"));
+
+                if (ActivityCompat.checkSelfPermission(AboutUs.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                startActivity(callIntent);
+            }
+        });
+
+        email_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, email_1.getText().toString());
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        email_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                intent.putExtra(Intent.EXTRA_EMAIL, email_2.getText().toString());
+                if (intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+    }@Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+    public void setUpToolbar() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+        actionBarDrawerToggle.syncState();
+    }
+
+
+}
