a6de1b50cd497de5c9782c48686fd0e6376a14fb
1-ARIjitS <arijits19@iiserb.ac.in>
2021-03-20 01:55:54 +0530

    suspect registration activity completed fully with list view post recycler yet to be completed

--- app/src/main/java/com/example/womensafety/LocationConstants.java
+++ app/src/main/java/com/example/womensafety/LocationConstants.java
@@ -4,7 +4,7 @@ class LocationConstants {
 
     static final int LOCATION_SERVICE_ID = 175;
     static final String ACTION_START_LOCATION_SERVICE = "startLocationService";
-    static final String ACTION_STOP_LOCATION_SERVICE  = "stopLocationService";
+    static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationService";
 
 
 }
