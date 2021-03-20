596cd3072033d51a63235ad24c0b050e54f8de1b
Dev Kumar <64922424+rDevKumar@users.noreply.github.com>
2021-03-20 08:13:45 +0530

    Update Detail_Forms.java

--- app/src/main/java/com/example/womensafety/Detail_Forms.java
+++ app/src/main/java/com/example/womensafety/Detail_Forms.java
@@ -14,11 +14,14 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.TimePicker;
@@ -35,17 +38,18 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
-import com.example.womensafety.Activities.AboutUsActivity;
 import com.example.womensafety.Activities.AdminActivity;
-import com.example.womensafety.Activities.LoginActivity;
 import com.example.womensafety.Activities.NextToKinActivity;
-import com.example.womensafety.Activities.NextTokinListActivity;
-import com.example.womensafety.Activities.SuspectListActivity;
 import com.example.womensafety.Activities.SuspectRegistrationActivity;
+import com.example.womensafety.Activities.TravelLog;
 import com.google.android.material.navigation.NavigationView;
-import com.google.firebase.auth.FirebaseAuth;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class Detail_Forms extends AppCompatActivity {
 
@@ -54,12 +58,10 @@ public class Detail_Forms extends AppCompatActivity {
     String format;
     Calendar calender;
     TimePickerDialog timePickerDialog;
+
     Button VehicleImageSelector;
     public static final int GET_FROM_GALLERY = 3;
     ImageView imageView;
-    TextView timeTextView;
-
-    FirebaseAuth auth;
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
@@ -67,8 +69,16 @@ public class Detail_Forms extends AppCompatActivity {
 
     TextView tv_latitude, tv_longitude;
     Double latitude, longitude;
+    EditText et_vehicleNumber, et_travellingTo, et_travellingFrom;
+    TextView tv_estimatedTime;
 
     private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
+
+    Intent intent;
+
+    //Travel Log passing values
+    String vehicleNumber, travellingTo, travellingFrom, timeStarted, timeReached, address, date, estimatedTime;
+    Uri vehicleImageUri;
 
 
     @SuppressLint("SetTextI18n")
@@ -79,21 +89,35 @@ public class Detail_Forms extends AppCompatActivity {
 
         tv_latitude = findViewById(R.id.tv_latitude);
         tv_longitude = findViewById(R.id.tv_longitude);
+        tv_estimatedTime = findViewById(R.id.estimatedTime);
 
-        auth = FirebaseAuth.getInstance();
+        et_vehicleNumber = findViewById(R.id.vehicleNumber);
+        et_travellingFrom = findViewById(R.id.travelingFrom);
+        et_travellingTo = findViewById(R.id.travelingTo);
+
+        vehicleNumber = et_vehicleNumber.getText().toString();
+        travellingFrom = et_travellingFrom.getText().toString();
+        travellingTo = et_travellingTo.getText().toString();
 
 
         findViewById(R.id.startButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                timeStarted = getCurrentTime();
+
+                calender = Calendar.getInstance();
+                date = DateFormat.getDateInstance(DateFormat.FULL).format(calender.getTime());
+
                 if (ContextCompat.checkSelfPermission(
                         getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
                     ActivityCompat.requestPermissions(
-                            Detail_Forms.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            Detail_Forms.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                             REQUEST_CODE_LOCATION_PERMISSION
                     );
-                } else {
+                }
+                else{
                     startLocationService();
                 }
             }
@@ -103,11 +127,16 @@ public class Detail_Forms extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 stopLocationService();
+                timeReached = getCurrentTime();
+                address = getLocation();
+                vehicleNumber = et_vehicleNumber.getText().toString();
+                travellingFrom = et_travellingFrom.getText().toString();
+                travellingTo = et_travellingTo.getText().toString();
+                fn_reachedSafely();
             }
         });
 
         Access_DisplayTime = findViewById(R.id.timePicker);
-        timeTextView = findViewById(R.id.timeTextView);
 
 
         Access_DisplayTime.setOnClickListener(new View.OnClickListener() {
@@ -131,10 +160,11 @@ public class Detail_Forms extends AppCompatActivity {
                         } else {
                             format = "AM";
                         }
-                        timeTextView.setVisibility(View.VISIBLE);
-                        timeTextView.setText("Estimated time: " + hourOfDay + ":" + minute + " " + format);
+                        tv_estimatedTime.setVisibility(View.VISIBLE);
+                        tv_estimatedTime.setText("Estimated time: " + hourOfDay + ":" + minute + " " + format);
+                        estimatedTime = hourOfDay + ":" + minute + " " + format;
                     }
-                }, CalenderHour, CalenderMinute, false);
+                }, CalenderHour, CalenderMinute, false );
                 timePickerDialog.show();
             }
         });
@@ -163,12 +193,15 @@ public class Detail_Forms extends AppCompatActivity {
                         break;
 
                     case R.id.nav_suspectRegistration:
-                        startActivity(new Intent(Detail_Forms.this, SuspectListActivity.class));
+                        startActivity(new Intent(Detail_Forms.this, SuspectRegistrationActivity.class));
                         break;
                     case R.id.nav_nextToKin:
-                        startActivity(new Intent(Detail_Forms.this, NextTokinListActivity.class));
+                        startActivity(new Intent(Detail_Forms.this, NextToKinActivity.class));
                         break;
-
+                    case R.id.nav_travelLog:
+                        startActivity(new Intent(Detail_Forms.this, TravelLog.class));
+                        break;
+                       
                     case R.id.nav_aboutUs:
                         startActivity(new Intent(Detail_Forms.this, AboutUsActivity.class));
                         break;
@@ -193,10 +226,25 @@ public class Detail_Forms extends AppCompatActivity {
             longitude = Double.valueOf(intent.getStringExtra("longitude"));
 
 
-            tv_latitude.setText(latitude + "");
-            tv_longitude.setText(longitude + "");
+            tv_latitude.setText(latitude+"");
+            tv_longitude.setText(longitude+"");
         }
     };
+
+    private void fn_reachedSafely(){
+
+        intent = new Intent(Detail_Forms.this, TravelLog.class);
+        intent.putExtra("vehicleNumber", vehicleNumber+"");
+        intent.putExtra("travellingFrom", travellingFrom+"");
+        intent.putExtra("travellingTo", travellingTo+"");
+        intent.putExtra("date", date+"");
+        intent.putExtra("timeStarted", timeStarted+"");
+        intent.putExtra("timeReached", timeReached+"");
+        intent.putExtra("address", address+"");
+        intent.putExtra("vehicleImageUri", vehicleImageUri);
+        intent.putExtra("estimatedTime", estimatedTime+"");
+        startActivity(intent);
+    }
 
     @Override
     protected void onResume() {
@@ -210,26 +258,27 @@ public class Detail_Forms extends AppCompatActivity {
         unregisterReceiver(broadcastReceiver);
     }
 
-    @Override
+        @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 startLocationService();
-            } else {
-                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(this,"Permission denied",Toast.LENGTH_SHORT).show();
             }
         }
     }
 
-    private boolean isLocationServiceRunning() {
+    private boolean isLocationServiceRunning(){
         ActivityManager activityManager =
                 (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        if (activityManager != null) {
-            for (ActivityManager.RunningServiceInfo service :
-                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
-                if (LocationService.class.getName().equals(service.service.getClassName())) {
-                    if (service.foreground) {
+        if(activityManager != null){
+            for(ActivityManager.RunningServiceInfo service:
+            activityManager.getRunningServices(Integer.MAX_VALUE)){
+                if(LocationService.class.getName().equals(service.service.getClassName())){
+                    if(service.foreground){
                         return true;
                     }
                 }
@@ -239,36 +288,39 @@ public class Detail_Forms extends AppCompatActivity {
         return false;
     }
 
-    private void startLocationService() {
-        if (!isLocationServiceRunning()) {
+    private void startLocationService(){
+        if(!isLocationServiceRunning()){
             Intent intent = new Intent(getApplicationContext(), LocationService.class);
             intent.setAction(LocationConstants.ACTION_START_LOCATION_SERVICE);
             startService(intent);
-            Toast.makeText(this, "Tracking Starts", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Tracking Starts",Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void stopLocationService() {
-        if (isLocationServiceRunning()) {
+    private void stopLocationService(){
+        if(isLocationServiceRunning()){
             Intent intent = new Intent(getApplicationContext(), LocationService.class);
             intent.setAction(LocationConstants.ACTION_STOP_LOCATION_SERVICE);
             startService(intent);
-            Toast.makeText(this, "Tracking Stops", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this,"Tracking Stops",Toast.LENGTH_SHORT).show();
         }
     }
 
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode, resultCode, data);
 
 
-        if (requestCode == GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+        if(requestCode == GET_FROM_GALLERY && resultCode == Activity.RESULT_OK){
 
             imageView = findViewById(R.id.vehicleImage);
             imageView.setVisibility(View.VISIBLE);
             imageView.setImageURI(data.getData());
+            vehicleImageUri = data.getData();
         }
     }
+
 
 
     @Override
@@ -279,7 +331,6 @@ public class Detail_Forms extends AppCompatActivity {
             super.onBackPressed();
         }
     }
-
     public void setUpToolbar() {
         drawerLayout = findViewById(R.id.drawerLayout);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -288,6 +339,23 @@ public class Detail_Forms extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
         actionBarDrawerToggle.syncState();
+    }
+
+    private String getCurrentTime(){
+        return new SimpleDateFormat("hh:mm", Locale.getDefault()).format(new Date());
+    }
+
+    private String getLocation(){
+
+        try{
+            Geocoder geocoder = new Geocoder(Detail_Forms.this, Locale.getDefault());
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+            String address = addresses.get(0).getAddressLine(0);
+            return address;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
