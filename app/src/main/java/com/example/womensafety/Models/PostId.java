282bd6db0d49605a29ff43bfbaff401827ad76ad
1-ARIjitS <arijits19@iiserb.ac.in>
2021-03-27 21:08:41 +0530

    constraint layout added in place of linear layout in order to support rotation of device in suspect registration list  and next to kin list and manifest updated to remove screen orientation tags to support both potrait and landscape operations

--- app/src/main/java/com/example/womensafety/Models/PostId.java
+++ app/src/main/java/com/example/womensafety/Models/PostId.java
@@ -9,7 +9,7 @@ public class PostId {
     @Exclude
       public String PostId;
 
-    public <T extends PostId> T withId(@NonNull final String id)
+    public <T extends PostId> T withId ( @NonNull final String id )
     {
         this.PostId=id;
         return (T)this;
